deb3161f6e94ed25deda2b5e14dda667560219d6
John Flynn <mcflynnthm@gmail.com>
2020-12-16 15:36:31 -0500

    Command framework setup, non functional mostly

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-
 # bot.py
 # 2020 mcflynnthm
 
 ## import Discord.py elements
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands
 
 ## import dotenv
 from dotenv import load_dotenv
@@ -21,7 +20,7 @@ lastrun_time = datetime.now()
 TOKEN=os.getenv("DISCORD_TOKEN")
 
 ## Create bot obj
-client = Bot(command_prefix="!")
+client = commands.Bot(command_prefix='!')
 
 ## define function for on_ready
 @client.event
@@ -34,17 +33,11 @@ async def on_message(message):
    global lastrun_time
    ## Print trigger message to server console
    #print(message.author.name+" said, '"+message.content+"'")
-   #myChannel=client.get_channel(788165007161950248)
    myChannel=message.channel
    if not message.author.id == 788165007161950248:
       temp_time = datetime.now()
       diff = temp_time - lastrun_time
-      if "!greeting" in message.content.lower():
-         await message.channel.send("Of greetings...")
-      elif "!goodbye" in message.content.lower():
-         await message.channel.send("...and goodbyes.")
-         ## who doesn't like AFI amirite
-      elif "bosstones" in message.content.lower():
+      if "bosstones" in message.content.lower():
          if diff.seconds > 300:
             ## update lastrun_time to new run time
             lastrun_time = temp_time
@@ -54,6 +47,32 @@ async def on_message(message):
          ## apply reaction if exists on server
          if mmb_emoji:
             await message.add_reaction(mmb_emoji)
+   await client.process_commands(message)
+
+@client.command(name='mute')
+async def mute(ctx):
+   await ctx.send("The Bot is quiet now.")
+
+@client.command(name='muteuntil', help="HELP: tell the bot how many minutes you want it to stop yammering for.")
+async def muteuntil(ctx, arg):
+   if not arg:
+      pass
+   elif arg.len() == 0:
+      pass
+   else:
+      await ctx.send("The Bot will be quiet until "+arg)
+
+@client.command(name='wake')
+async def wake(ctx):
+   await ctx.send("The Bot has awoken. Despair.")
+
+@client.command(name='greeting')
+async def greeting(ctx):
+   await ctx.send("Of greetings...")
+
+@client.command(name='goodbye')
+async def goodbye(ctx):
+   await ctx.send("...and goodbyes.")
 
 ## let's start the machine
 client.run(TOKEN)
